9f0b4913dc5fa13f74bcf4a3a94165e9c68bcb0f
takidog <wow@taki.dog>
2020-07-25 01:44:04 +0800

    Add web ap scores example

---
+++ example/scores.dart
@@ -0,0 +1,13 @@
+import 'package:nkust_api/src/webap.dart';
+
+void main() async {
+  NKUST_API.instance.setProxy("127.0.0.1:8888");
+  var res = await NKUST_API.instance.apLogin("username", "password");
+
+  if (res.errorCode == 1000) {
+    var s = await NKUST_API.instance.scores("108", "2");
+    if (s.errorCode >= 2000 && s.errorCode <= 2100) {
+      print(s.parseData);
+    }
+  }
+}
