13c3cde4425165163e9997f51ea00694a15d9cd9
takidog <wow@taki.dog>
2020-07-24 21:55:45 +0800

    Add user info parser

--- lib/src/parser/apParser.dart
+++ lib/src/parser/apParser.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart';
 
 int apLoginParser(String html) {
   //100 login success
@@ -8,6 +10,26 @@ int apLoginParser(String html) {
     return 100;
   }
   return 101;
+}
+
+Map<String, dynamic> apUserInfoParser(String html) {
+  Map<String, dynamic> data = new Map();
+  var document = parse(html);
+  String image_url =
+      document.getElementsByTagName("img")[0].attributes["src"].substring(2);
+  data['educationSystem'] =
+      (document.getElementsByTagName("td")[3].text.replaceAll("學　　制：", ""));
+  data['department'] =
+      (document.getElementsByTagName("td")[4].text.replaceAll("科　　系：", ""));
+  data['className'] =
+      (document.getElementsByTagName("td")[8].text.replaceAll("班　　級：", ""));
+  data['id'] =
+      (document.getElementsByTagName("td")[9].text.replaceAll("學　　號：", ""));
+  data['name'] =
+      (document.getElementsByTagName("td")[10].text.replaceAll("姓　　名：", ""));
+  data['pictureUrl'] = "https://webap.nkust.edu.tw/nkust${image_url}";
+
+  return data;
 }
 
 void main() {
