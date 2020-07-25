c5dda63bfaec8ea8ee0acd1b218e3c5f875d882a
takidog <wow@taki.dog>
2020-07-25 14:23:25 +0800

    Add web ap room list crawler

--- lib/src/webap.dart
+++ lib/src/webap.dart
@@ -287,4 +287,29 @@ class NKUST_API {
     }
     return query;
   }
+
+  Future<ResponseData> roomList(String cmpAreaId) async {
+    /*
+    Retrun type ResponseData
+    errorCode:
+    2000   succss.
+
+    5000   NKUST server error.
+    5002   Dio error, maybe NKUST server error.
+    5040   Timeout.
+    5400   Something error.
+
+    cmpAreaId
+    1=建工/2=燕巢/3=第一/4=楠梓/5=旗津
+    */
+    if (this.isLogin == false) {
+      return ResponseData(errorCode: 10002, errorMessage: "Need Login.");
+    }
+    var query = await apQuery("ag302_01", {"cmp_area_id": cmpAreaId});
+    if (query.errorCode >= 2000 && query.errorCode < 2100) {
+      query.parseData = roomListParser(query.response.data);
+      return query;
+    }
+    return query;
+  }
 }
